bca91fac3ddeeb591f0e7ccb46587ca1e7c4ec13
Pillin <ignacia.pp@gmail.com>
2016-01-14 16:39:25 -0300

    add fix placeCaretAtEnd for all major browser

--- wagtailembedder/static/wagtailembedder/js/hallo-embedder.js
+++ wagtailembedder/static/wagtailembedder/js/hallo-embedder.js
@@ -1,5 +1,21 @@
 (function() {
   (function($) {
+    function placeCaretAtEnd(el) {
+      el.focus();
+      if ( typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+      } else if ( typeof document.body.createTextRange != "undefined" ) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+      }
+    }
     return $.widget("IKS.halloembedder", {
       options: {
         uuid: '',
